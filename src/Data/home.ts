1780351e48c6bff17328d2efc9adf3043da1a147
Desdevises Simon <simon.desdevises@gmail.com>
2023-11-17 15:08:42 +0100

    fixed data

--- src/Data/home.ts
+++ src/Data/home.ts
@@ -9,11 +9,11 @@ export const welcome = {
       "L'école d'ingénieur polytechnique de l'Université de Lille, membre du réseau Polytech qui regroupe 15 écoles d'ingénieur polytechniques universitaires publiques en France et 100 000 ingénieurs en activité.\n" +
       "Notre école, publique, est accessible en cycle ingénieur (3 ans) à bac+2 à de multiples profils mais également directement après le bac grâce à un parcours original de 2 ans, le Parcours des écoles d'ingénieurs Polytech.\n" +
       "9 spécialités d'ingénieur sont proposées à Polytech Lille, sous statut étudiant mais également apprenti et 100 au niveau des écoles Polytech.",
+    english:
+      "The Polytechnic engineering school of the University of Lille, a member of the Polytech network which brings together 15 public university polytechnic engineering schools in France and 100,000 engineers in activity.\n" +
+      "Our school, public, is accessible in engineering cycle (3 years) at bac + 2 to multiple profiles but also directly after the bac thanks to an original course of 2 years, the Course of Polytech engineering schools.\n" +
+      "9 engineering specialties are offered at Polytech Lille, under student status but also apprentice and 100 at the Polytech schools level.",
   },
-  english:
-    "The Polytechnic engineering school of the University of Lille, a member of the Polytech network which brings together 15 public university polytechnic engineering schools in France and 100,000 engineers in activity.\n" +
-    "Our school, public, is accessible in engineering cycle (3 years) at bac + 2 to multiple profiles but also directly after the bac thanks to an original course of 2 years, the Course of Polytech engineering schools.\n" +
-    "9 engineering specialties are offered at Polytech Lille, under student status but also apprentice and 100 at the Polytech schools level.",
   rightContent: {
     french:
       "Rencontrons-nous ! Nos rendez-vous, nos contacts\n" +
